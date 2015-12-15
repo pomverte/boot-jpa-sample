a4506bc34d6545737122f1dafec7ecf4e83f1310
pomverte <vietnem@yahoo.fr>
2015-12-15 13:38:39 +0100

    encapsulate response body into ResponseEntity

--- src/main/java/com/pomverte/controller/PersonController.java
+++ src/main/java/com/pomverte/controller/PersonController.java
@@ -3,6 +3,8 @@ package com.pomverte.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,8 +21,8 @@ public class PersonController {
     private PersonService personService;
 
     @RequestMapping(value = "/findByRank/{rank}", method = RequestMethod.GET)
-    public List<PersonDto> findByRank(@PathVariable Long rank) {
-        return this.personService.findByRank(rank);
+    public ResponseEntity<List<PersonDto>> findByRank(@PathVariable Long rank) {
+        return new ResponseEntity<List<PersonDto>>(this.personService.findByRank(rank), HttpStatus.OK);
     }
 
 }
